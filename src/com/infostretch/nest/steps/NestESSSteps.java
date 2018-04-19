645c7d1d3dd4c4b4599dbc614931750ee04a0b86
Prashinka Sahu <prashinka.sahu@infostretch.com>
2018-04-19 10:00:28 +0530

    Added few changes

--- src/com/infostretch/nest/steps/NestESSSteps.java
+++ src/com/infostretch/nest/steps/NestESSSteps.java
@@ -2,19 +2,15 @@ package com.infostretch.nest.steps;
 
 import javax.ws.rs.core.MediaType;
 
-import org.hamcrest.Matchers;
 import org.json.JSONObject;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import com.infostretch.nest.providers.ESSEndPoints;
 import com.infostretch.nest.utils.ClientUtils;
+import com.infostretch.nest.utils.CommonUtils;
 import com.infostretch.nest.utils.TokenUtils;
-import com.qmetry.qaf.automation.core.ConfigurationManager;
 import com.qmetry.qaf.automation.core.MessageTypes;
 import com.qmetry.qaf.automation.step.QAFTestStep;
 import com.qmetry.qaf.automation.util.Reporter;
-import com.qmetry.qaf.automation.util.Validator;
 import com.qmetry.qaf.automation.ws.Response;
 
 public class NestESSSteps {
@@ -26,13 +22,9 @@ public class NestESSSteps {
 		obj.put("emp_number", "113");//ConfigurationManager.getBundle().getPropertyValue("emp_id");
 		ClientUtils.getWebResource(ESSEndPoints.GET_BASIC_DETAILS).type(MediaType.APPLICATION_JSON).post(obj.toString());
 		Response response = ClientUtils.getResponse();
-		Reporter.log(response.getMessageBody(), MessageTypes.Info);
-		JsonObject responseBody =
-				new JsonParser().parse(response.getMessageBody()).getAsJsonObject();
-		JsonObject results = responseBody.get("response").getAsJsonObject().get("results")
-				.getAsJsonObject();
-		Validator.verifyThat(results, Matchers.notNullValue());
-		
+		if(CommonUtils.validateResult(response)) {
+			Reporter.log(response.getMessageBody(), MessageTypes.Info);
+		}
 	}
 
 	@QAFTestStep(description = "user should get-personal-details")
@@ -42,10 +34,9 @@ public class NestESSSteps {
 		obj.put("emp_number", "113");//ConfigurationManager.getBundle().getPropertyValue("emp_id");
 		ClientUtils.getWebResource(ESSEndPoints.GET_PERSONAL_DETAILS).type(MediaType.APPLICATION_JSON).post(obj.toString());
 		Response response = ClientUtils.getResponse();
-		Reporter.log(response.getMessageBody(), MessageTypes.Info);
-		JsonObject responseBody = new JsonParser().parse(response.getMessageBody()).getAsJsonObject();
-		JsonObject results = responseBody.get("response").getAsJsonObject().get("results").getAsJsonObject();
-		Validator.verifyThat(results, Matchers.notNullValue());
+		if(CommonUtils.validateResult(response)) {
+			Reporter.log(response.getMessageBody(), MessageTypes.Info);
+		}
 	}
 	
 
