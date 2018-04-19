c5da4d677a1babe9f0cb028a81740cbeac016618
Bhakti Kulkarni <bhakti.kulkarni@infostretch.com>
2018-04-19 11:38:06 +0530

    commit

--- src/com/infostretch/nest/steps/NestHomeSteps.java
+++ src/com/infostretch/nest/steps/NestHomeSteps.java
@@ -8,6 +8,7 @@ import com.infostretch.nest.providers.EndPoints;
 import com.infostretch.nest.utils.ClientUtils;
 import com.infostretch.nest.utils.TokenUtils;
 import com.qmetry.qaf.automation.core.MessageTypes;
+import com.qmetry.qaf.automation.step.NotYetImplementedException;
 import com.qmetry.qaf.automation.step.QAFTestStep;
 import com.qmetry.qaf.automation.util.Reporter;
 import com.qmetry.qaf.automation.util.Validator;
@@ -61,6 +62,42 @@ public class NestHomeSteps {
 		Reporter.log(response.getMessageBody(), MessageTypes.Info);
 		Validator.assertThat(response.getStatus().getStatusCode(), Matchers.equalTo(200));
 	}
+	@QAFTestStep(description = "User should get leave balances")
+	public void userShouldGetLeaveBalances() {
+		ClientUtils.getWebResource(EndPoints.GET_LEAVE_BALANCES).entity(TokenUtils.getTokenAsJsonStr())
+		.type(MediaType.APPLICATION_JSON).post();
+		Response response = ClientUtils.getResponse();
+		Reporter.log(response.getMessageBody(), MessageTypes.Info);
+		Validator.assertThat(response.getStatus().getStatusCode(), Matchers.equalTo(200));	
+	}
+	
+	@QAFTestStep(description = "User should get floating holiday list")
+	public void userShouldGetFloatingHolidayList() {
+		ClientUtils.getWebResource(EndPoints.GET_FLOATING_HOLIDAY_LIST).entity(TokenUtils.getTokenAsJsonStr())
+		.type(MediaType.APPLICATION_JSON).post();
+		Response response = ClientUtils.getResponse();
+		Reporter.log(response.getMessageBody(), MessageTypes.Info);
+		Validator.assertThat(response.getStatus().getStatusCode(), Matchers.equalTo(200));		
+	}
+	
+	@QAFTestStep(description = "User should get holiday list")
+	public void userShouldGetHolidayList() {
+		ClientUtils.getWebResource(EndPoints.GET_HOLIDAY_LIST).entity(TokenUtils.getTokenAsJsonStr())
+		.type(MediaType.APPLICATION_JSON).post();
+		Response response = ClientUtils.getResponse();
+		Reporter.log(response.getMessageBody(), MessageTypes.Info);
+		Validator.assertThat(response.getStatus().getStatusCode(), Matchers.equalTo(200));	
+	}
+	
+	@QAFTestStep(description = "User should get training calendar list")
+	public void userShouldGetTrainingCalendarList() {
+		ClientUtils.getWebResource(EndPoints.GET_TRAINING_CALENDAR_LIST).entity(TokenUtils.getTokenAsJsonStr())
+		.type(MediaType.APPLICATION_JSON).post();
+		Response response = ClientUtils.getResponse();
+		Reporter.log(response.getMessageBody(), MessageTypes.Info);
+		Validator.assertThat(response.getStatus().getStatusCode(), Matchers.equalTo(200));	
+	}
+
 }
 	
 	
