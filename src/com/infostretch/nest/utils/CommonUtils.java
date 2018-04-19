8cff017bbbe48a35e297cb2fae0cde9438dead79
Prashinka Sahu <prashinka.sahu@infostretch.com>
2018-04-19 10:11:23 +0530

    Commit

--- src/com/infostretch/nest/utils/CommonUtils.java
+++ src/com/infostretch/nest/utils/CommonUtils.java
@@ -7,11 +7,22 @@ import com.qmetry.qaf.automation.util.Validator;
 import com.qmetry.qaf.automation.ws.Response;
 public class CommonUtils {
 
-	public static boolean validateResposneParameter(Response response, String parameterName) {
+
+	public static boolean validateResult(Response response) {
 		JsonObject responseBody = new JsonParser().parse(response.getMessageBody()).getAsJsonObject();
 		JsonObject results = responseBody.get("response").getAsJsonObject().get("results").getAsJsonObject();
 		Validator.assertThat(results, Matchers.notNullValue());
-		Validator.assertThat(results.get(parameterName).getAsString(), Matchers.notNullValue());
 		return true;
+	}
+	
+	
+	
+	public static String validateResultParameter(Response response, String parameterName) {
+		JsonObject responseBody = new JsonParser().parse(response.getMessageBody()).getAsJsonObject();
+		JsonObject results = responseBody.get("response").getAsJsonObject().get("results").getAsJsonObject();
+		Validator.assertThat(results, Matchers.notNullValue());
+		String ParameterValue =results.get(parameterName).getAsString();
+		Validator.assertThat(ParameterValue, Matchers.notNullValue());
+		return ParameterValue;
 	}
 }
