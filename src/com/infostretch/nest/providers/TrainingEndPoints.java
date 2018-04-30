5010d1fdc29af8653722172c3ec73648650dae74
Suryakant.Kale <suryakant.kale@infostretch.com>
2018-04-30 15:31:09 +0530

    Done with changes at 3:30pm 4/30/2018

--- src/com/infostretch/nest/providers/TrainingEndPoints.java
+++ src/com/infostretch/nest/providers/TrainingEndPoints.java
@@ -6,8 +6,16 @@ public interface TrainingEndPoints {
 	public static final String I_AM_Attending = "/get-i-am-attending";
 	public static final String I_HAVE_ATTENDED = "/get-i-have-attended";
 	public static final String I_HAVE_TAUGHT = "/get-i-have-taught";
-	public static final String GET_I_AM_TEACHING="/get-i-am-teaching";
-	public static final String GET_FEEDBACK_QUESTIONS_LIST="/get-feedback-questions-list";
-	
+	public static final String GET_I_AM_TEACHING = "/get-i-am-teaching";
+	public static final String GET_FEEDBACK_QUESTIONS_LIST =
+			"/get-feedback-questions-list";
+
+	public static final String GET_COURSE_LIST = "/get-course-list";
+	public static final String GET_STATISTICS = "/get-statistics";
+	public static final String GET_TRAINING_TYPE_DD = "/get-training-type-dd";
+	public static final String GET_INDIVIDUAL_EMP_STATISTICS =
+			"/get-individual-emp-statistics";
+	public static final String GET_VENUE_LIST = "/get-venue-list";
+	public static final String ADD_EDIT_VENUE = "/add-edit-venue";
 
 }
